8456cf8501aebaa691a4d893a257bbac5624b3bc
selectdev <selectdev01@gmail.com>
2025-06-03 18:25:05 +0000

    PARSED SOMETHING. NOT SURE IF IT WAS MY CODE OR MY SANITY

---
+++ zevola.go
@@ -0,0 +1,32 @@
+package main
+
+import "github.com/purrquinox/zevola/core"
+
+var Stop = core.Stop
+var Config = core.Config
+var OnEvent = core.OnEvent
+var Respond = core.Respond
+var SendMessage = core.SendMessage
+var EditMessage = core.EditMessage
+var DeferInteraction = core.DeferInteraction
+var GetUserAvatarURL = core.GetUserAvatarURL
+var ParseCommand = core.ParseCommand
+var LogEvent = core.LogEvent
+var IsBotEvent = core.IsBotEvent
+var GetAuthor = core.GetAuthor
+var GetChannelID = core.GetChannelID
+var NewCommand = core.NewCommand
+var NewOption = core.NewOption
+var PaginateReply = core.PaginateReply
+var FindCommandByName = core.FindCommandByName
+var EnsureSlashCommands = core.EnsureSlashCommands
+var GetGuildID = core.GetGuildID
+var GetUsername = core.GetUsername
+var IsAdmin = core.IsAdmin
+var Cooldown = core.Cooldown
+var Retry = core.Retry
+var Background = core.Background
+var Start = core.Start
+var Revolt = core.Revolt
+var Discord = core.Discord
+var SetStatus = core.SetStatus
